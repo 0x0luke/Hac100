939ea40426b13776229a49f966c87be02ef83cc3
Luke <lukejcw@icloud.com>
2017-10-29 00:12:26 +0100

    fuck my life and fuck git

--- probability.php
+++ probability.php
@@ -4,12 +4,15 @@ $userid = $_GET['userid'];
 $productid = $_GET['productid'];
 $basket = $_GET['basket'];
 
-$basketcomp = array($basket);
+parse_str($basket, $basketARR);
+echo $basketARR['basket'];
+echo $basketARR[','][0];
+
 $url = 'http://127.0.0.1:3000/items';
 
 $products = array('tea' => 'coffee', 'milk' => 'sugar');
 
-$compare = array_diff($basketcomp,$products);
+$compare = array_diff_assoc($basketARR,$products);
 
 $data = array('productID' => $productid, 'userid' => $userid, 'suggested' => $compare, 'probability' => $probability);
 $postdata = http_build_query($data);
