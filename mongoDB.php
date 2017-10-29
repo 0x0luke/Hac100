fd9a3c98c50199f5016394d358a87c85848902c3
Luke <lukejcw@icloud.com>
2017-10-29 05:36:09 +0000

    something

--- mongoDB.php
+++ mongoDB.php
@@ -7,7 +7,7 @@ $m = new MongoClient();
 $db = $m->selectDB('Tododb');
 $collection = new MongoCollection($db, 'Tododb');
 
-
+echo "connect tings";
 $lookupprod = array('ProductID' => $productID, 'userid' => $userid);
 
 $mathmedaddy = $lookupprod/rand(0, 3);
