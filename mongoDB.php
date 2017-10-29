4f0daed39e95529cec4810c15a29113d4ecfbe2f
Luke <lukejcw@icloud.com>
2017-10-29 05:34:35 +0000

    kill me

--- mongoDB.php
+++ mongoDB.php
@@ -1,8 +1,17 @@
 <?php
 
+$productID = $_GET['productID'];
+$userid = $_GET['userid'];
+
 $m = new MongoClient();
-$db = $m->selectDB('test');
+$db = $m->selectDB('Tododb');
 $collection = new MongoCollection($db, 'Tododb');
 
+
+$lookupprod = array('ProductID' => $productID, 'userid' => $userid);
+
+$mathmedaddy = $lookupprod/rand(0, 3);
+
+echo $mathmedaddy;
 
 ?>
